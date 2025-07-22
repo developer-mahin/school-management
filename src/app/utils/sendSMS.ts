2b3d9d2816af3927c7ebe3c543068430949aaf37
developer-mahin <mdmahin1310@gmail.com>
2025-07-22 11:20:36 +0600

    update send message

--- src/app/utils/sendSMS.ts
+++ src/app/utils/sendSMS.ts
@@ -9,25 +9,52 @@ import config from '../../config';
 const agent = new https.Agent({ rejectUnauthorized: false });
 
 const sendSMS = async (payload: { phoneNumber: string; message: string }) => {
-  const data = {
-    username: config.kwt_sms.username,
-    password: config.kwt_sms.password,
-    sender: config.kwt_sms.sender,
-    mobile: payload.phoneNumber,
-    lang: 1,
-    message: payload.message,
-  };
+  // const data = {
+  //   username: config.kwt_sms.username,
+  //   password: config.kwt_sms.password,
+  //   sender: config.kwt_sms.sender,
+  //   mobile: payload.phoneNumber,
+  //   lang: 1,
+  //   message: payload.message,
+  // };
+
+  // try {
+  //   const response = await axios.post(`${config.kwt_sms.kwt_sms_url}`, data, {
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     httpsAgent: agent,
+  //   });
+
+  //   console.log('SMS sent successfully:', response.data);
+
+  //   return response.data;
+  // } catch (error: any) {
+  //   console.error('SMS sending failed:', error.response?.data || error.message);
+  //   throw new AppError(httpStatus.BAD_REQUEST, error);
+  // }
+
+  const params = new URLSearchParams();
+  params.append('username', `${config.kwt_sms.username}`);
+  params.append('password', `${config.kwt_sms.password}`);
+  params.append('sender', `${config.kwt_sms.sender}`);
+  params.append('mobile', payload.phoneNumber);
+  params.append('lang', '1');
+  params.append('message', payload.message);
 
   try {
-    const response = await axios.post(`${config.kwt_sms.kwt_sms_url}`, data, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+    const response = await axios.post(
+      `https://www.kwtsms.com/API/send/`,
+      params.toString(), // Serialize as string
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        httpsAgent: agent,
       },
-      httpsAgent: agent,
-    });
+    );
 
     console.log('SMS sent successfully:', response.data);
-
     return response.data;
   } catch (error: any) {
     console.error('SMS sending failed:', error.response?.data || error.message);
